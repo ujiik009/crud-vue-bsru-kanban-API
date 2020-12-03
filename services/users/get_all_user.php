2c492af575af0d0f1a2ee46401ef7148b65a6f74
ikool009 <ikool009@hotmail.com>
2020-12-04 01:44:14 +0700

    -

---
+++ services/users/get_all_user.php
@@ -0,0 +1,59 @@
+<?php
+include "../../database/database.php";
+include "../../helper/helper_jwt.php";
+$return = array();
+
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    $headers = apache_request_headers();
+
+    if (isset($headers['Authorization'])) {
+        $token = $headers['Authorization'];
+
+        try {
+            if (decode_jwt($token)) {
+
+                $database = new Database();
+                //receive JSON POST
+                $json = file_get_contents('php://input');
+                // Converts it into a PHP object
+                $body = json_decode($json, true);
+
+                $sql = "
+                SELECT * FROM users
+                ";
+
+                $result = mysqli_query($database->getConnection(), $sql);
+
+                if ($result) {
+                    $rows = [];
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        unset($row["password"]);
+                        $rows[] = $row;
+                    }
+                    $return["status"] = true;
+                    $return["data"] = $rows;
+                } else {
+                    $return["status"] = false;
+                    $return["message"] = mysqli_error($database->getConnection());
+                }
+            } else {
+                $return["status"] = false;
+                $return["message"] = "Token incorrect";
+            }
+        } catch (Throwable $err) {
+            $return["status"] = false;
+            $return["message"] = "Token incorrect";
+        }
+    } else {
+        $return["status"] = false;
+        $return["message"] = "Token Not Found";
+    }
+} else {
+    $return["status"] = "Method not allow";
+}
+
+
+header('Content-Type: application/json');
+echo json_encode($return);
